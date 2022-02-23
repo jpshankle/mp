6c1a781635a8230f6a7739e3885633735d7d9db6
Jeremy Shankle <jpshankle@gmail.com>
2022-02-23 15:05:34 -0500

    Updates

---
+++ _next/static/rSyUajMH10IbdnXz-M1UF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-b3fce3532bd5046717f2.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
